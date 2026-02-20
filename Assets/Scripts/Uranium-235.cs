1009b996cb03244aa699fcd31d96f8e6d4fe868a
Viktor Popp <viktorpopp.dev@gmail.com>
2026-02-20 19:46:08 +0100

    made U-235 emit fast neutrons on fission

--- Assets/Scripts/Uranium-235.cs
+++ Assets/Scripts/Uranium-235.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class Uranium : MonoBehaviour
 {
-    public GameObject thermalNeutron;
+    public GameObject fastNeutron;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("ThermalNeutron"))
         {
-            Instantiate(thermalNeutron, this.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
-            Instantiate(thermalNeutron, this.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
-            Instantiate(thermalNeutron, this.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+            Instantiate(fastNeutron, this.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+            Instantiate(fastNeutron, this.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
+            Instantiate(fastNeutron, this.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
             Destroy(collision.gameObject);
             Destroy(this.gameObject);
         }
